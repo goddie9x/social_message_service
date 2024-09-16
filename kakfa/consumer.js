657d3f7886e92558a3feaa1676195646ca7a0ae4
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-16 19:57:48 +0700

    fix: create topic convention list topic is not an array of string but array of ITopicConfig

--- kakfa/consumer.js
+++ kakfa/consumer.js
@@ -1,8 +1,9 @@
-const { activeServiceConsumer } = require('../utils/kafka/consumer');
+const { activeServiceConsumer, createTopicIfNotExists } = require('../utils/kafka/consumer');
 const { KAFKA_TOPICS } = require('../utils/constants/kafka');
 const messageService = require('../services/messageService');
 
-const activeMessageServiceConsumer = async ()=>{
+const activeMessageServiceConsumer = async () => {
+    await createTopicIfNotExists([{ topic: KAFKA_TOPICS.MESSAGE_TOPIC.REQUEST }]);
     activeServiceConsumer({
         serviceInstance: messageService,
         topic: KAFKA_TOPICS.MESSAGE_TOPIC.REQUEST,
