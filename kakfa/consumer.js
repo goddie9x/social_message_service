f9344505c2a112fb91debd449740eda92bd8bf45
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-16 18:48:43 +0700

    refactor: migrate from kafka-node to kafkajs

--- kakfa/consumer.js
+++ kakfa/consumer.js
@@ -1,13 +1,8 @@
-const { activeServiceConsumer, createTopicIfNotExists } = require('../utils/kafka/consumer');
-const { kafkaClient } = require('../utils/kafka/producer');
+const { activeServiceConsumer } = require('../utils/kafka/consumer');
 const { KAFKA_TOPICS } = require('../utils/constants/kafka');
 const messageService = require('../services/messageService');
 
 const activeMessageServiceConsumer = async ()=>{
-    await createTopicIfNotExists({
-        topic: KAFKA_TOPICS.MESSAGE_TOPIC.REQUEST,
-        client: kafkaClient,
-    });
     activeServiceConsumer({
         serviceInstance: messageService,
         topic: KAFKA_TOPICS.MESSAGE_TOPIC.REQUEST,
